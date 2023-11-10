41fe264335e1442a6ad1459c216d7168de432757
Talaat Ramadan <talatramadan10@gmail.com>
2023-11-10 17:19:07 +0200

    Updated Program.cs to configure JWT Authentication

--- SohatNotebook.Api/Program.cs
+++ SohatNotebook.Api/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SohatNotebook.Api;
 using SohatNotebook.DataService.Data;
 using SohatNotebook.DataService.IConfiguration;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Update the JWt config from settings
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -28,6 +37,32 @@ builder.Services.AddApiVersioning(options =>
 
 });
 
+builder.Services.AddAuthentication(option =>
+{
+	option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+	option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+	.AddJwtBearer(jwt =>
+	{
+		var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfig:Secret"]);
+
+		jwt.SaveToken = true;
+		jwt.TokenValidationParameters = new TokenValidationParameters 
+		{ 
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(key),
+			ValidateIssuer = false, // ToDo Update
+			ValidateAudience = false, // ToDo Update
+			RequireExpirationTime = false, // ToDo Update
+			ValidateLifetime = true,
+		};
+	});
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options 
+		=> options.SignIn.RequireConfirmedAccount = true)
+	.AddEntityFrameworkStores<ApplicationDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,6 +74,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
