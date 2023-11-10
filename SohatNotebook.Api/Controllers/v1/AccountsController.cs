69fa0b026ef2760c824e41ed77d079603f345d61
Talaat Ramadan <talatramadan10@gmail.com>
2023-11-10 22:30:42 +0200

    Created Accounts Controller

---
+++ SohatNotebook.Api/Controllers/v1/AccountsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SohatNotebook.Authentication.Configruation;
+using SohatNotebook.DataService.IConfiguration;
+
+namespace SohatNotebook.Api.Controllers.v1
+{
+	public class AccountsController : BaseController
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly JwtConfig _jwt;
+		public AccountsController(IUnitOfWork unitOfWork,
+			UserManager<IdentityUser> userManager,
+			IOptionsMonitor<JwtConfig> jwt) : base(unitOfWork)
+		{
+			_jwt = jwt.CurrentValue;
+			_userManager = userManager;
+		}
+	}
+}
