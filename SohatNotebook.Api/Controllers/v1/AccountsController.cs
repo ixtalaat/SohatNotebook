835138706441a3931aa8251b805d00d19814eefb
Talaat Ramadan <talatramadan10@gmail.com>
2023-11-11 16:53:53 +0200

    Create JWT Generation Function

--- SohatNotebook.Api/Controllers/v1/AccountsController.cs
+++ SohatNotebook.Api/Controllers/v1/AccountsController.cs
@@ -1,22 +1,26 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using SohatNotebook.Authentication.Configruation;
 using SohatNotebook.Authentication.Models.DTO.Incoming;
 using SohatNotebook.Authentication.Models.DTO.Outcoming;
 using SohatNotebook.DataService.IConfiguration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SohatNotebook.Api.Controllers.v1
 {
 	public class AccountsController : BaseController
 	{
 		private readonly UserManager<IdentityUser> _userManager;
-		private readonly JwtConfig _jwt;
+		private readonly JwtConfig _jwtConfig;
 		public AccountsController(IUnitOfWork unitOfWork,
 			UserManager<IdentityUser> userManager,
-			IOptionsMonitor<JwtConfig> jwt) : base(unitOfWork)
+			IOptionsMonitor<JwtConfig> OptionMonitor) : base(unitOfWork)
 		{
-			_jwt = jwt.CurrentValue;
+			_jwtConfig = OptionMonitor.CurrentValue;
 			_userManager = userManager;
 		}
 
@@ -67,7 +71,38 @@ namespace SohatNotebook.Api.Controllers.v1
 				});
 			}
 		}
+		
+		private string GenerateJwtToken(IdentityUser user)
+		{
+			// the handler is going to be responsible for creating the token
+			var jwtHandler = new JwtSecurityTokenHandler();
 
+			// Get the security key
+			var key = Encoding.ASCII.GetBytes(_jwtConfig.Secret);
+
+			var tokenDescriptor = new SecurityTokenDescriptor 
+			{
+				Subject = new ClaimsIdentity(new[]
+				{
+					new Claim("Id", user.Id),
+					new Claim(JwtRegisteredClaimNames.Sub, user.Email), // unique id
+					new Claim(JwtRegisteredClaimNames.Email, user.Email),
+					new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) // used by the refresh token
+				}),
+				Expires = DateTime.UtcNow.AddHours(3), // Todo update the expiration time to minutes
+				SigningCredentials = new SigningCredentials(
+					new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256
+				)
+			};
+
+			// Generate the security obj token
+			var token = jwtHandler.CreateToken(tokenDescriptor);
+
+			// Convert the security obj token into a string
+			var jwtToken = jwtHandler.WriteToken(token);
+
+			return jwtToken;
+		}
 
 	}
 }
